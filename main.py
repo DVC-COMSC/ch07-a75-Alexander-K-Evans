e8f687d6f33ea9f6e50a238ceba3c9c0a9794862
Alexander Evans <kingevans22202@gmail.com>
2022-10-24 14:48:35 -0700

    complete

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ import sys
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
 
+flag = False
+
 if len(num1) > len(num2):
 	print ('False')
 	sys.exit(0)
-# ******************************
-# Make your Code
-# ******************************
+
+for i in range(len(num2)):
+	if num1[0] == num2[i]:
+		flag = True
+		if len(num2) - len(num1) >= i:
+			for j in range(len(num1)):
+				if num1[j] != num2[i+j]:
+					flag = False
+		else:
+			flag = False
+print(flag)
+
 
 # print ('True') or ('False')
